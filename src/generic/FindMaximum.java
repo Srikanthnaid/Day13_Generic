f7c0e9eaa279dd8101468eac58483493b670974a
Srikanthnaid <ksru044gmail.com>
2022-11-22 14:10:27 +0530

    Refactor all the 3 to one Generic method and find the maximum

--- src/generic/FindMaximum.java
+++ src/generic/FindMaximum.java
@@ -1,27 +1,34 @@
 package generic;
 
-public class FindMaximum {
-	
-	// Method with parameters as float data type
-	public static String findMax(String a,String b,String c) {
-		
-		String max = "" ;
-		// comparing numbers using compareTo method.
-		if(a.compareTo(b)>0 && a.compareTo(c)>0) {
-			max = a;
-		}else if(b.compareTo(a)>0 && b.compareTo(c)>0) {
-			max = b;
-		}else if(c.compareTo(a)>0 && c.compareTo(b)>0) {
-			max = c;
-		}
+public class FindMaximum<T extends Comparable<T>> {
+
+	// Class Based Variables
+	T a1, a2, a3;
+
+	// Constructor
+	public FindMaximum(T a1, T a2, T a3) {
+
+		this.a1 = a1;
+		this.a2 = a2;
+		this.a3 = a3;
+	}
+
+	// Create Generic Method
+	public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+		T max = a1;
+		if (a2.compareTo(max) > 0)
+			max = a2;
+		if (a3.compareTo(max) > 0)
+			max = a3;
 		return max;
-		
 	}
 
 	public static void main(String[] args) {
-		
-		FindMaximum maximum = new FindMaximum();
-		System.out.println("Maximum of among 3 Values is "+findMax("abc", "ghi", "def"));
+
+		System.out.println("Welcome To Find Maximun Problem Using Generics \n");
+		System.out.println("The maximum value between the three integer is : " + maxOfValues(3, 8, 5) + "\n");
+		System.out.println("The maximum value between the three float is : " + maxOfValues(1.5f, 3.56f, 5.87f) + "\n");
+		System.out.println("The maximum value between the three string is : " + maxOfValues("abc", "pqr", "xyz"));
 
 	}
 

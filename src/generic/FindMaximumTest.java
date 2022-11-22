268add5fe987408e76e0a33e500fddf4acff0511
B Srikanth <110146597+Srikanthnaid@users.noreply.github.com>
2022-11-22 15:29:17 +0530

    Update FindMaximumTest.java

--- src/generic/FindMaximumTest.java
+++ src/generic/FindMaximumTest.java
@@ -5,12 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class FindMaximumTest {
-	static FindMaximum findMaximum;
-
+	
 	@Test
 	void findMaximum_Of_3Integers() {
-		findMaximum = new FindMaximum();
-		Integer currentMood = findMaximum.findMax(5, 8, 2);
+		FindMaximum findMaximum = new FindMaximum();
+		assertEquals(8, findMaximum.findMax(5,8,2));
 	}
-
 }

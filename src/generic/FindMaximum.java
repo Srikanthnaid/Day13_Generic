0e9c588d074837382ccd14c0735b5b7871f27124
B Srikanth <110146597+Srikanthnaid@users.noreply.github.com>
2022-11-22 17:55:25 +0530

    Update FindMaximum.java

--- src/generic/FindMaximum.java
+++ src/generic/FindMaximum.java
@@ -5,21 +5,22 @@ public class FindMaximum {
 	// Method with parameters as Integer
 	public static Integer findMax(Integer a,Integer b,Integer c) {
 		
-		int max = 0;
+		int max = a;//assume initially a is maximum
 		// comparing numbers using compareTo method.
-		if(a.compareTo(b)>0 && a.compareTo(c)>0) {
-			max = a;
-		}else if(b.compareTo(a)>0 && b.compareTo(c)>0) {
+		if (b.compareTo(max) > 0 ) {
 			max = b;
-		}else if(c.compareTo(a)>0 && c.compareTo(b)>0) {
+		}
+		if (c.compareTo(max) > 0) {
 			max = c;
 		}
+		
 		return max ;
 	}
 
 	public static void main(String[] args) {
 		
-		FindMaximum maximum = new FindMaximum();
+		//FindMaximum maximum = new FindMaximum();
+		//here need not create object becase method in static.
 		System.out.println("Maximum of among 3 integers is "+findMax(5, 8, 2));
 
 	}

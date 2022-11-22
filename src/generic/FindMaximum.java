fa2dcc2f520fdb3f76d7706d9dc10add0474f43e
Srikanthnaid <ksru044gmail.com>
2022-11-22 14:16:12 +0530

    Added Refactor code

--- src/generic/FindMaximum.java
+++ src/generic/FindMaximum.java
@@ -5,14 +5,6 @@ public class FindMaximum<T extends Comparable<T>> {
 	// Class Based Variables
 	T a1, a2, a3;
 
-	// Constructor
-	public FindMaximum(T a1, T a2, T a3) {
-
-		this.a1 = a1;
-		this.a2 = a2;
-		this.a3 = a3;
-	}
-
 	// Create Generic Method
 	public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
 		T max = a1;

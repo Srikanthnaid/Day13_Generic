4bbdc4c718b1f3d2cdca1f7f35b511d83a66f264
Srikanthnaid <ksru044gmail.com>
2022-11-22 12:48:34 +0530

    Find maximum ineteger among 3 integers

--- src/generic/FindMaximum.java
+++ src/generic/FindMaximum.java
@@ -5,21 +5,22 @@ public class FindMaximum {
 	// Method with parameters as Integer
 	public static Integer findMax(Integer a,Integer b,Integer c) {
 		
+		int max = 0;
 		// comparing numbers using compareTo method.
 		if(a.compareTo(b)>0 && a.compareTo(c)>0) {
-			System.out.println("Maximum of "+a);
+			max = a;
 		}else if(b.compareTo(a)>0 && b.compareTo(c)>0) {
-			System.out.println("Maximum of "+b);
+			max = b;
 		}else if(c.compareTo(a)>0 && c.compareTo(b)>0) {
-			System.out.println("Maximum of "+c);
+			max = c;
 		}
-		return c ;
+		return max ;
 	}
 
 	public static void main(String[] args) {
 		
 		FindMaximum maximum = new FindMaximum();
-		maximum.findMax(5, 8, 2);
+		System.out.println("Maximum of among 3 integers is "+findMax(5, 8, 2));
 
 	}
 

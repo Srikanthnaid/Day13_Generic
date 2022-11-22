55c4dbdc648af9e971c165ad9fbbfa742cc1e2cb
B Srikanth <110146597+Srikanthnaid@users.noreply.github.com>
2022-11-22 18:05:13 +0530

    Update FindMaximum.java

--- src/generic/FindMaximum.java
+++ src/generic/FindMaximum.java
@@ -5,16 +5,16 @@ public class FindMaximum {
 	// Method with parameters as float data type
 	public static Float findMax(Float a,Float b,Float c) {
 		
-		Float max = 0.0f;
+		Float max = a;//assume initially a is maximum
 		// comparing numbers using compareTo method.
-		if(a.compareTo(b)>0 && a.compareTo(c)>0) {
-			max = a;
-		}else if(b.compareTo(a)>0 && b.compareTo(c)>0) {
+		if (b.compareTo(max) > 0 ) {
 			max = b;
-		}else if(c.compareTo(a)>0 && c.compareTo(b)>0) {
+		}
+		if (c.compareTo(max) > 0) {
 			max = c;
 		}
-		return max ;
+		
+		return max;
 	}
 
 	public static void main(String[] args) {

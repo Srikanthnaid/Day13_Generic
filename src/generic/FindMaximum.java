2a76380c4b78d9c39a675a6785206d45754c3bb5
Srikanthnaid <ksru044gmail.com>
2022-11-22 13:54:04 +0530

    Firn maximum String amoung 3 values of String

--- src/generic/FindMaximum.java
+++ src/generic/FindMaximum.java
@@ -3,9 +3,9 @@ package generic;
 public class FindMaximum {
 	
 	// Method with parameters as float data type
-	public static Float findMax(Float a,Float b,Float c) {
+	public static String findMax(String a,String b,String c) {
 		
-		Float max = 0.0f;
+		String max = "" ;
 		// comparing numbers using compareTo method.
 		if(a.compareTo(b)>0 && a.compareTo(c)>0) {
 			max = a;
@@ -14,13 +14,14 @@ public class FindMaximum {
 		}else if(c.compareTo(a)>0 && c.compareTo(b)>0) {
 			max = c;
 		}
-		return max ;
+		return max;
+		
 	}
 
 	public static void main(String[] args) {
 		
 		FindMaximum maximum = new FindMaximum();
-		System.out.println("Maximum of among 3 integers is "+findMax(5.5f, 8.25f, 2.0f));
+		System.out.println("Maximum of among 3 Values is "+findMax("abc", "ghi", "def"));
 
 	}
 
--- src/generic/FindMaximumTest.java
+++ src/generic/FindMaximumTest.java
@@ -5,12 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class FindMaximumTest {
-	static FindMaximum findMaximum;
 
 	@Test
 	void findMaximum_Of_3Integers() {
-		findMaximum = new FindMaximum();
-		Float currentMood = findMaximum.findMax(5.5f, 8.5f, 2.0f);
+		FindMaximum findMaximum = new FindMaximum();
+		String currentMood = findMaximum.findMax("abc", "ghi", "def");
 	}
 
 }
